d0e45bead91000903cd714ee6b4f3d6349572fe1
WY-OwO <jjv_2014@hotmail.com>
2026-03-05 00:11:09 -0800

    added tabContent2 js code

--- tabJava.js
+++ tabJava.js
@@ -19,3 +19,24 @@ function openCity(evt, cityName) {
   evt.currentTarget.className += " active";
 }
 
+function openChar(evt, charName) {
+  // Declare all variables
+  var i, tabContent2, tablinks2;
+
+  // Get all elements with class="tabContent" and hide them
+  tabContent2 = document.getElementsByClassName("tabContent2");
+  for (i = 0; i < tabContent2.length; i++) {
+    tabContent2[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks2 = document.getElementsByClassName("tablinks2");
+  for (i = 0; i < tablinks2.length; i++) {
+    tablinks2[i].className = tablinks2[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(charName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
